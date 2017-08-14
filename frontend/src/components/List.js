2f597b8dc5d23dddc1313e22f2f9a898287de7f4
Ian Liu <ian1654@gmail.com>
2017-08-14 15:08:35 +0800

    Can drag card onto empty list on client

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { DragDropContext } from 'react-dnd';
+import { DragDropContext, DropTarget } from 'react-dnd';
+import ItemTypes from './ItemTypes';
 import HTML5Backend from 'react-dnd-html5-backend';
 
 import Card from '../containers/Card';
@@ -59,21 +60,53 @@ class List extends Component {
   }
 
   render() {
-    return (
-      <Panel header={this.panelHeader()} bsStyle="info">
-        <ListGroup>
-          {this.props.cards.map((card, i) => (
-            <Card key={card.id} index={i} {...card} />
-          ))}
-        </ListGroup>
-        <AddCard list={this.props} />
-      </Panel>
+    return this.props.connectDropTarget(
+      <div>
+        <Panel header={this.panelHeader()} bsStyle="info">
+          <ListGroup>
+            {this.props.cards.map((card, i) => (
+              <Card key={card.id} index={i} {...card} />
+            ))}
+          </ListGroup>
+          <AddCard list={this.props} />
+        </Panel>
+      </div>
     )  
   }
 }
 
+// Drag Target
+const listTarget = {
+  hover(props, monitor, component) {
+    const dragList = monitor.getItem().list;
+    const dragIndex = monitor.getItem().index;
+
+    const hoverList = props.id;
+
+    if (dragList === hoverList) {
+      return;
+    }
+
+    if (props.cards.length > 0) {
+      return;
+    }
+
+    props.moveCardToEmptyList(dragList, dragIndex, hoverList);
+
+    monitor.getItem().list = hoverList;
+    monitor.getItem().index = 0;
+  }
+};
+
+const targetCollect = connect => ({
+  connectDropTarget: connect.dropTarget()
+});
+
+List = DropTarget(ItemTypes.CARD, listTarget, targetCollect)(List);
+
 List.propTypes = {
   onDeleteList: PropTypes.func.isRequired,
+  moveCardToEmptyList: PropTypes.func.isRequired,
   cards: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
--- frontend/src/containers/List.js
+++ frontend/src/containers/List.js
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux';
-import { deleteList } from '../actions';
+import { deleteList, moveCard } from '../actions';
 import List from '../components/List';
 
 const mapDispatchToProps = dispatch => ({
   onDeleteList: list => {
     dispatch(deleteList(list));
+  },
+  moveCardToEmptyList: (dragList, dragIndex, hoverList) => {
+    console.log(dragList + ":" + dragIndex + " to " + hoverList + ":0");
+    dispatch(moveCard(dragList, dragIndex, hoverList, 0));
   }
 })
 
