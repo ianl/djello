94efd7118bb0df11ee79049da3a791a19d66b7d1
Ian Liu <ian1654@gmail.com>
2017-08-17 16:10:16 +0800

    Install and setup PostgreSQL

--- backend/djello/settings.py
+++ backend/djello/settings.py
@@ -82,13 +82,23 @@ WSGI_APPLICATION = 'djello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djello',
+        'USER': 'ianthl',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
