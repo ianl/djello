b6b96a85f23095266ee098b438889855ed64d53d
Ian Liu <ian1654@gmail.com>
2017-08-23 16:26:58 +0800

    Set cards_order default to an empty list

--- backend/lists/models.py
+++ backend/lists/models.py
@@ -4,7 +4,7 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class List(models.Model):
     name = models.CharField(max_length=100)
-    cards_order = ArrayField(models.IntegerField(), blank=True, null=True)
+    cards_order = ArrayField(models.IntegerField(), default=list)
 
     board = models.ForeignKey('boards.Board', on_delete=models.CASCADE, related_name='lists')
 
